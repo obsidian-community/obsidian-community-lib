91b28eae5a12c7d925efe6f7f7e7c60851d89761
Ross Keenan <rossk29@gmail.com>
2021-10-24 09:53:46 +0200

    feat: :sparkles: Add `copy` function

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,15 @@
+import { Notice } from "obsidian";
+
 export function sayHello() {
   console.log("hi");
 }
 export function sayGoodbye() {
   console.log("goodbye");
+}
+
+export async function copy(content: string) {
+  await navigator.clipboard.writeText(content).then(
+    () => new Notice("Copied to clipboard"),
+    () => new Notice("Could not copy to clipboard")
+  );
 }
