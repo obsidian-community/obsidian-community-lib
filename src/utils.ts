14ab90e56ba0a4eb2559d92be0d37762362543b2
Ross Keenan <rossk29@gmail.com>
2021-10-24 12:28:48 +0200

    feat: :sparkles: Add my functions

--- src/utils.ts
+++ src/utils.ts
@@ -113,13 +113,148 @@ export function getAvailablePathForAttachments(
 }
 
 /**
- * Copy Text to the Users Clipboard.
- * 
- * @param content The contend to be copied to clipboard.
+ * Copy `content` to the users clipboard.
+ *
+ * @param {string} content The content to be copied to clipboard.
+ * @param {() => any} success The callback to run when text is successfully copied. Default throws a new `Notice`
+ * @param {(reason?) => any} failure The callback to run when text was not able to be copied. Default throws a new `Notice`, and console logs the error.`
  */
-export async function copy(content: string) {
-  await navigator.clipboard.writeText(content).then(
-    () => new Notice("Copied to clipboard"),
-    () => new Notice("Could not copy to clipboard")
-  );
+export async function copy(
+  content: string,
+  success: () => any = () => new Notice("Copied to clipboard"),
+  failure: (reason?: any) => any = (reason) => {
+    new Notice("Could not copy to clipboard");
+    console.log({ reason });
+  }
+) {
+  await navigator.clipboard.writeText(content).then(success, failure);
+}
+
+/**
+ * Given an editor, check if something is selected and return that selection, otherwise return the entire content of the editor
+ * @param  {Editor} editor
+ */
+export function getSelectionFromEditor(editor: Editor): string {
+  if (editor.somethingSelected()) {
+    return editor.getSelection();
+  } else {
+    return editor.getValue();
+  }
+}
+
+/**
+ * Check if something is selected in the current file and return that selection, otherwise return the entire content of the current file.
+ * @param  {App} app
+ * @param  {boolean} [cached=true] Use `cachedRead` or `read`. `cachedRead` by default.
+ */
+export async function getSelectionFromCurrFile(
+  app: App,
+  cached: boolean = true
+): Promise<string> {
+  const text = window?.getSelection()?.toString();
+  if (text) {
+    return text;
+  } else {
+    const currFile = app.workspace.getActiveFile();
+    if (currFile instanceof TFile) {
+      if (cached) {
+        return await app.vault.cachedRead(currFile);
+      } else {
+        return await app.vault.read(currFile);
+      }
+    } else {
+      new Notice("You must be focused on a markdown file.");
+    }
+  }
+}
+/**
+ * Check if `noteName` is the name of a note that exists in the vault.
+ * @param  {App} app
+ * @param  {string} noteName Basename of the note to search for.
+ * @param  {string} [sourcePath=""] Optional file path to start searching from. Default is the current file.
+ * @returns boolean
+ */
+export const isInVault = (
+  app: App,
+  noteName: string,
+  sourcePath: string = ""
+): boolean => !!app.metadataCache.getFirstLinkpathDest(noteName, sourcePath);
+
+/**
+ * @param  {MouseEvent} event
+ * @param  {TView} view The view being hovered
+ * @param  {string} to The basename of the note to preview
+ * @returns void
+ */
+export function hoverPreview<TView extends View>(
+  event: MouseEvent,
+  view: TView,
+  to: string
+): void {
+  const targetEl = event.target as HTMLElement;
+
+  view.app.workspace.trigger("hover-link", {
+    event,
+    source: view.getViewType(),
+    hoverParent: view,
+    targetEl,
+    linktext: to,
+  });
+}
+
+/**
+ * When clicking a link, check if that note is already open in another leaf, and switch to that leaf, if so. Otherwise, open the note in a new pane
+ * @param  {App} app
+ * @param  {string} dest Basename of note to open to open
+ * @param  {MouseEvent} event
+ * @param  {{createNewFile:boolean}} [options={createNewFile:true}]
+ * @returns Promise
+ */
+export async function openOrSwitch(
+  app: App,
+  dest: string,
+  event: MouseEvent,
+  options: {
+    createNewFile: boolean;
+  } = { createNewFile: true }
+): Promise<void> {
+  const { workspace } = app;
+  const currFile = workspace.getActiveFile();
+  let destFile = app.metadataCache.getFirstLinkpathDest(dest, currFile.path);
+
+  // If dest doesn't exist, make it
+  if (!destFile) {
+    if (!options.createNewFile) return;
+    const newFileFolder = app.fileManager.getNewFileParent(currFile.path).path;
+    const newFilePath = normalizePath(
+      `${newFileFolder}${newFileFolder === "/" ? "" : "/"}${dest}.md`
+    );
+    await app.vault.create(newFilePath, "");
+    destFile = app.metadataCache.getFirstLinkpathDest(
+      newFilePath,
+      currFile.path
+    );
+  }
+
+  // Check if it's already open
+  const leavesWithDestAlreadyOpen: WorkspaceLeaf[] = [];
+  // For all open leaves, if the leave's basename is equal to the link destination, rather activate that leaf instead of opening it in two panes
+  workspace.iterateAllLeaves((leaf) => {
+    if (leaf.view?.file?.basename === dest) {
+      leavesWithDestAlreadyOpen.push(leaf);
+    }
+  });
+
+  // Rather switch to it if it is open
+  if (leavesWithDestAlreadyOpen.length > 0) {
+    workspace.setActiveLeaf(leavesWithDestAlreadyOpen[0]);
+  } else {
+    const mode = (app.vault as any).getConfig("defaultViewMode");
+    const leaf =
+      event.ctrlKey || event.getModifierState("Meta")
+        ? workspace.splitActiveLeaf()
+        : workspace.getUnpinnedLeaf();
+
+    await leaf.openFile(destFile, { active: true, mode });
+  }
 }
