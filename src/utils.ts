4c3040cdaab4185752d09ced4765891b020df58c
Ross Keenan <rossk29@gmail.com>
2021-11-09 21:35:48 +0200

    fix: :bug: openOrSwitch incorrect logic checking if user wants to createNewFile

--- src/utils.ts
+++ src/utils.ts
@@ -244,7 +244,7 @@ export const stripMD = (noteName: string): string => {
 /**
  * When clicking a link, check if that note is already open in another leaf, and switch to that leaf, if so. Otherwise, open the note in a new pane.
  * @param  {App} app
- * @param  {string} dest Basename of note to open to open
+ * @param  {string} dest Basename of note to open
  * @param  {MouseEvent} event
  * @param  {{createNewFile:boolean}} [options={createNewFile:true}] Whether or not to create `dest` file if it doesn't exist. If `false`, simply return from the function.
  * @returns Promise
@@ -264,7 +264,7 @@ export async function openOrSwitch(
   // If dest doesn't exist, make it
   if (!destFile && options.createNewFile) {
     destFile = await createNewMDNote(app, destStripped);
-  } else if (!destFile && options.createNewFile) return;
+  } else if (!destFile && !options.createNewFile) return;
 
   // Check if it's already open
   const leavesWithDestAlreadyOpen: WorkspaceLeaf[] = [];
